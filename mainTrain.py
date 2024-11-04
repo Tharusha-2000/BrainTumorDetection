3b3b26208ebc4c8da47b37c81c3a53212e7a4bda
tharushadinuth(pro) <tharushadinuth21@gmail.com>
2024-11-04 22:10:38 +0530

    minor changers

--- mainTrain.py
+++ mainTrain.py
@@ -11,10 +11,10 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.utils import to_categorical
 
-image_directory='datasets/'
+image_directory='Alldataset/datasets/'
 
-no_tumor_images=os.listdir(image_directory+ 'no/')
-yes_tumor_images=os.listdir(image_directory+ 'yes/')
+no_tumor_images=os.listdir(image_directory+ 'no')
+yes_tumor_images=os.listdir(image_directory+ 'yes')
 dataset=[]
 label=[]
 
